2f2625d069a4c3213fcadaba02191db509941dcc
Kamye <andrej15ganzha@gmail.com>
2016-10-29 05:49:00 +0300

    do commit

--- DatingChat/js/scripts.js
+++ DatingChat/js/scripts.js
@@ -21,5 +21,27 @@ $(document).ready(function () {
             }
         });
     });
-    
+
+    $('.slider').slick();
+
+    $(function($) {
+        var allAccordions = $('.accordion div.data');
+        var allAccordionItems = $('.accordion .accordion-item');
+        $('.accordion > .accordion-item').click(function() {
+            if($(this).hasClass('open'))
+            {
+                $(this).removeClass('open');
+                $(this).next().slideUp("slow");
+            }
+            else
+            {
+                allAccordions.slideUp("slow");
+                allAccordionItems.removeClass('open');
+                $(this).addClass('open');
+                $(this).next().slideDown("slow");
+                return false;
+            }
+        });
+    });
+
 });
